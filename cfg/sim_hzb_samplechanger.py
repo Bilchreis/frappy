7700587415b4c9eb6e21426be97d8e8ae3d82c31
Peter <peter.braun@helmholtz-berlin.de>
2025-02-24 10:46:38 +0100

    copied samplechenger classes

---
+++ cfg/sim_hzb_samplechanger.py
@@ -0,0 +1,44 @@
+nsamples = 12
+
+Node('sample_changer.HZB',  # a globally unique identification
+     'Sample Changer\n\nThis is an demo for a  SECoP (Sample Environment Communication Protocol) sample changer SEC-Node.',  # describes the node
+      'tcp://10803',
+      implementor = 'Peter Wegmann')  # you might choose any port number > 1024
+
+ 
+    
+Mod('hardware',
+    'frappy_HZB.hardware.hardware',
+    'The hardware component responsible for physically moving the samples',
+
+
+    
+    model = "none",
+    serial = "none",
+    ur_version = "none",
+    
+   
+    pollinterval = 0.1,
+
+)
+
+Mod('storage',
+    'frappy_HZB.storage.Storage',
+    'Hardware component that holds a number of samples in sample slots',
+
+    a_sample = 'sample_at_measurement_position',
+    a_hardware = 'hardware',
+    pollinterval = 1
+  
+)
+
+
+Mod('sample_at_measurement_position',
+    'frappy_HZB.special_position.Special_Position',
+    'Sample currently present at the measuerement position',
+
+    a_hardware = 'hardware',
+    a_storage = 'storage',
+    pollinterval = 1,
+
+    )
